331c10a2ae9a8387ac48dc5770a7135212297d57
Matt <matt.grenander@gmail.com>
2020-01-19 17:44:51 -0500

    ready for testing

--- src/helper.py
+++ src/helper.py
@@ -1,5 +1,9 @@
 import pickle
 import numpy as np
+import re
+import torch
+
+REMOVE_CHAR_PATTERN = re.compile('[^A-Za-z0-9]')
 
 def process_text(tok_client, text):
     ann = tok_client.annotate(text)
@@ -12,12 +16,23 @@ def process_text(tok_client, text):
     return text_sents
 
 
-def strip_punc(text):
-    pass
+def strip_punc(tokens):
+    stripped = []
+    for t in tokens:
+        if not REMOVE_CHAR_PATTERN.match(t):
+            stripped += re.sub(REMOVE_CHAR_PATTERN, " ", t.lower()).split()
+    return stripped
 
 
-def convert_tokens_to_ids(sents):
-    pass
+def convert_tokens_to_ids(doc, args):
+    max_len = len(max(doc, key=lambda x: len(x)))
+    sent_list = []
+    for i in range(len(doc)):
+        words = doc[i]
+        sent = [args.word2id[word] if word in args.word2id else 1 for word in words]
+        sent += [0 for _ in range(max_len - len(sent))]  # this is to pad at the end of each sequence
+        sent_list.append(sent)
+    return torch.tensor(sent_list).long()
 
 
 def config(args, vocab):
@@ -46,7 +61,7 @@ class Vocab():
         with open(vocab_file, "r") as f:
             for line in f:
                 self.word_list.append(line.split()[0])  # only want the word, not the count
-        print("read {} words from vocab file".format(len(self.word_list)))
+        print("Read {} words from vocab file".format(len(self.word_list)))
 
         for w in self.word_list:
             self.w2i[w] = self.count
@@ -68,7 +83,7 @@ class Vocab():
                     model[word] = embedding
                     embedding_matrix[self.w2i[word]] = embedding
                     if len(model) % 1000 == 0:
-                        print("processed {} data".format(len(model)))
+                        print("Processed {} data".format(len(model)))
         self.embedding = embedding_matrix
         print("%d words out of %d has embeddings in the glove file".format((len(model), len(self.word_list))))
 
